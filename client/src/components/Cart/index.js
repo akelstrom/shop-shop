f3b1f51061017930efdd2be3e0f986f8a7edd5ec
akelstrom <akelstrom@gmail.com>
2021-04-03 14:33:47 -0400

    added redux store, refacored the reducers, and set up pages

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -2,18 +2,23 @@ import React, { useEffect } from "react";
 import CartItem from "../CartItem";
 import Auth from "../../utils/auth";
 import "./style.css";
-import { useStoreContext } from "../../utils/GlobalState";
+// import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 import { QUERY_CHECKOUT } from '../../utils/queries';
 import { loadStripe } from '@stripe/stripe-js';
 import { useLazyQuery } from '@apollo/react-hooks';
+//redux
+import { useDispatch, useSelector } from 'react-redux';
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
-    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
-  const [state, dispatch] = useStoreContext();
+  // const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+  // const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
+  const cartToggled = useSelector(state => state.cartOpen);
+  const cartItem = useSelector(state => state.cart)
   
   useEffect(() => {
     async function getCart() {
@@ -21,10 +26,10 @@ const Cart = () => {
       dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
     };
   
-    if (!state.cart.length) {
+    if (!cartItem.length) {
       getCart();
     }
-  }, [state.cart.length, dispatch]);
+  }, [cartItem.length, dispatch]);
 
   useEffect(() => {
     if (data) {
@@ -40,7 +45,7 @@ const Cart = () => {
 
   function calculateTotal() {
     let sum = 0;
-    state.cart.forEach(item => {
+    cartItem.forEach(item => {
       sum += item.price * item.purchaseQuantity;
     });
     return sum.toFixed(2);
@@ -52,7 +57,7 @@ const Cart = () => {
         variables: { products: productIds }
       });
   
-    state.cart.forEach((item) => {
+    cartItem.forEach((item) => {
       for (let i = 0; i < item.purchaseQuantity; i++) {
         productIds.push(item._id);
       }
@@ -60,7 +65,7 @@ const Cart = () => {
     });
   }
 
-  if (!state.cartOpen) {
+  if (!cartToggled) {
     return (
       <div className="cart-closed" onClick={toggleCart}>
         <span
@@ -74,7 +79,7 @@ const Cart = () => {
     <div className="cart">
     <div className="close" onClick={toggleCart}>[close]</div>
     <h2>Shopping Cart</h2>
-    {state.cart.length ? (
+    {cartItem.length ? (
       <div>
         {state.cart.map(item => (
           <CartItem key={item._id} item={item} />
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -3,7 +3,7 @@ import { Link, useParams } from "react-router-dom";
 import { useQuery } from "@apollo/react-hooks";
 import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from "../assets/spinner.gif";
-import { useStoreContext } from "../utils/GlobalState";
+// import { useStoreContext } from "../utils/GlobalState";
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
@@ -12,16 +12,25 @@ import {
 } from "../utils/actions";
 import Cart from "../components/Cart";
 import { idbPromise } from "../utils/helpers";
+//redux
+import { useDispatch, useSelector } from 'react-redux';
+
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+  //redux changes
+  const dispatch = useDispatch();
+  const cart = useSelector(state => state.cart);
+  const products = useSelector(state => state.products);
+
+  // const [state, dispatch] = useStoreContext();
+
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const { products, cart } = state;
+  // const { products, cart } = state;
 
   useEffect(() => {
     // already in global store
